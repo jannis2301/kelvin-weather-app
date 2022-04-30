629503c3b871cbc2e0be9cbb3c60e8c83f3d9232
jannis2301 <jannis.ax@gmx.de>
2022-05-01 00:38:48 +0200

    Changed innerHTML to textContent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 const temperatureConverter = (kelvin) => {
   const celsius = kelvin - 273;
   fahrenheit = Math.floor((celsius * (9/5)) + 32);
-  document.getElementById("outputFahrenheit").innerHTML = fahrenheit;
+  document.getElementById("outputFahrenheit").textContent = fahrenheit;
 }
